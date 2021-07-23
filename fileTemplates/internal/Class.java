7d7f84ce7fc8a5b99c7658254d0fd501935d8de8
liuxuan <lX6272576>
2021-07-23 17:40:36 +0800

    IU-2021.1.2 <liuxuan@liuxuan Create baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, ide-features-trainer.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Default copy.xml, lombok-plugin.xml, yiiguxing.translation.xml, shared-indexes.xml, Default.xml, visualizationTool.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, colors.scheme.xml, databaseDrivers.xml, github.xml, templates.xml, ClassNotes.xml, output.xml, Default.xml, laf.xml, parameter.hints.xml, docker.xml, Class.java, jdk.table.xml, path.macros.xml, wsl.distributions.xml, vcs.xml, codota.xml, editor.codeinsight.xml, toStringTemplates.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/** 
+* 深圳云路信息科技有限责任公司 版权所有 © Copyright 2021
+* @Description: 
+* @Author: 刘轩
+* @Date: ${DATE}
+*/
+public class ${NAME} {
+}
